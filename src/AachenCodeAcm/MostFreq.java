c5d79d1473a66b34f0546bf592b87ebeae54e113
Ahmed Mousa <Ahmed.Mousa98@eng-st.cu.edu.eg>
2022-05-10 13:29:31 +0200

    added the second 2 examples of hello2

---
+++ src/AachenCodeAcm/MostFreq.java
@@ -0,0 +1,29 @@
+package AachenCodeAcm;
+
+import java.io.Console;
+import java.util.*;
+
+public class MostFreq {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String y = in.nextLine();
+        int x = Integer.valueOf(y);
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        for (int i = 0; i < x; i++) {
+            String word = in.nextLine();
+            int val = map.getOrDefault(word, 0);
+
+            if (val == 0) {
+                map.put(word, 1);
+            } else if (val > 0) {
+                map.put(word, ++val);
+            }
+        }
+
+        //System.out.println(map.get("mido"));
+        System.out.println(map.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey());
+
+    }
+}
